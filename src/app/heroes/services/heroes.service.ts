c4da7c05d7c4ac67dde0d91a7bda8fc34eea3203
xander98in <ledyerin@unicauca.edu.co>
2024-05-01 02:53:34 -0500

    Environments y servicio de heroes

---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environments } from '../../../environments/environments';
+import { Observable } from 'rxjs';
+import { Hero } from '../interfaces/hero.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroesService {
+
+  private baseUrl: string = environments.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
+  }
+
+}
